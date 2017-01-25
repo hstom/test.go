9e6a4737254d5708ff40eab70c3b0538e9507cb1
Thomas Coladonato <thomas.coladonato@hotschedules.com>
2017-01-24 22:27:08 -0600

    120fps 16 colors add windows exit hotkey

--- test.go
+++ test.go
@@ -7,8 +7,12 @@ import (
 
 	"github.com/jroimartin/gocui"
 )
-
+//                                     ,-windows precision
+//                                    |  ,-mac precision
+//                                    v v
 var tFmt = "2006-01-02 15:04:05.000000000 -0700 MST"
+
+var visited = make([][]bool, 8)
 
 func main() {
 	g, err := gocui.NewGui(gocui.OutputNormal)
@@ -18,40 +22,99 @@ func main() {
 
 	defer g.Close()
 
+	for i := range visited {
+		visited[i] = make([]bool, 8)
+		for j := range visited[i] {
+			visited[i][j] = false
+		}
+	}
+
 	g.SetManagerFunc(layout)
 
+	//TODO would be nice to set exit hotkey based on OS detection
 	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
 
-	t1 := time.NewTicker(time.Millisecond * 100)
-	t2 := time.NewTicker(time.Millisecond * 350)
+	if err := g.SetKeybinding("", gocui.KeyCtrlW, gocui.ModNone, quit); err != nil {
+		log.Panicln(err)
+	}
+
+	targetFps := int64(120)
+	duration := time.Duration(time.Second.Nanoseconds() / targetFps)*time.Nanosecond
+	tick := time.NewTicker(duration)
+	last := time.Now()
+
+	steps := 0
+	f := uint8(7)
+	b := uint8(7)
 
 	go func() {
-		for t := range t1.C {
+		for t := range tick.C {
+			b = (b + 1) % 8
+
+			if b == 0 {
+				f = (f + 1) % 8
+			}
+
+			if b == f {
+				b = (b + 1) % 8
+				if b == 0 {
+					f = 1
+					b = 0
+				}
+			}
+
+			visited[b][f] = true
+
+			g.Execute(func(g *gocui.Gui) error {
+				v, err := g.View("t2")
+				if err != nil {
+					log.Panicln(err)
+				}
+				colorFPrintln(v, t.Format(tFmt), f, b)
+				return nil
+			})
+
 			g.Execute(func(g *gocui.Gui) error {
 				v, err := g.View("t1")
 				if err != nil {
 					log.Panicln(err)
 				}
 				v.Clear()
-				fmt.Fprintln(v, t.Format(tFmt))
-				return nil
-			})
-		}
-	}()
 
-	go func() {
-		for t := range t2.C {
-			g.Execute(func(g *gocui.Gui) error {
-				v, err := g.View("t2")
-				if err != nil {
-					log.Panicln(err)
+				denom := t.Sub(last).Nanoseconds()
+				var fps interface{} = "INFINITY"
+				if denom != 0{
+					fps = (time.Second.Nanoseconds()/denom)
 				}
-				v.Clear()
-				fmt.Fprintln(v, t.Format(tFmt))
+				fmt.Fprintln(v, f, b,"TARGET FPS:", targetFps, "FRAMELENGTH:", duration)
+				fmt.Fprintln(v, "APPROX FPS:", fps)
+				last = t
 				return nil
 			})
+
+			if steps < 64 {
+				steps += 1
+				g.Execute(func(g *gocui.Gui) error {
+					v, err := g.View("grid")
+					if err != nil {
+						log.Panicln(err)
+					}
+					v.Clear()
+					for i := range visited {
+						for j := range visited[i] {
+							if visited[i][j] {
+								colorFPrint(v, "▮", uint8(j), uint8(i))
+							} else {
+								fmt.Fprint(v, " ")
+							}
+						}
+						fmt.Fprint(v, "\n")
+					}
+					return nil
+				})
+			}
 		}
 	}()
 
@@ -60,19 +123,36 @@ func main() {
 	}
 }
 
+func colorFPrint(v *gocui.View, string string, f, b uint8) {
+	fmt.Fprintf(v, "\033[3%dm\033[4%dm%s\033[0m", f, b, string)
+}
+func colorFPrintln(v *gocui.View, string string, f, b uint8) {
+	//Prepend newline so that autoscroll doesn't advance prematurely
+	fmt.Fprintf(v, "\n\033[3%dm\033[4%dm%s\033[0m", f, b, string)
+}
+
 func layout(g *gocui.Gui) error {
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("t1", maxX/2-20, maxY/2, maxX/2+20, maxY/2+2); err != nil {
+	if v, err := g.SetView("t1", maxX/2-20, maxY/2, maxX/2+30, maxY/2+3); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, time.Now().Format(tFmt))
+		v.Frame = false
 	}
-	if v, err := g.SetView("t2", maxX/2-20, maxY/4, maxX/2+20, maxY/4+2); err != nil {
+	if v, err := g.SetView("t2", maxX/2-20, 0, maxX/2+20, maxY/2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
-		fmt.Fprintln(v, time.Now().Format(tFmt))
+		v.Autoscroll = true
+		v.Frame = true
+		v.Title = "]TIME["
+	}
+	if v, err := g.SetView("grid", 0, 0, 9, 9); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		v.Frame = true
+		v.Title = "]SEEN["
 	}
 	return nil
 }
